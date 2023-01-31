e697e7eb48053b8508605ea8553d6e0f4091e1b2
7H3-PR073C70R <okanlawondamilare24434@gmail.com>
2023-01-31 21:00:26 +0200

    Api Endpoints

--- lib/src/core/constants/app_api_endpoint.dart
+++ lib/src/core/constants/app_api_endpoint.dart
@@ -4,13 +4,27 @@
 /// Example
 ///
 /// static Uri authUrl =
-///  Uri(scheme: 'https', host: 'oxygen.com', path: '/api/v1/auth')
+///  Uri(scheme: 'https', host: 'endpoint.com', path: '/api/v1/auth')
 class AppApiEndpoint {
   const AppApiEndpoint._();
   static const String scheme = 'https';
-  static const String host = '....com';
+  static const String host = 'firestore.googleapis.com';
   static const int receiveTimeout = 50000;
   static const int sendTimeout = 50000;
 
-  static Uri baseUri = Uri(scheme: scheme, host: host, path: '/api/v1');
+  static const _extension =
+      'v1/projects/applaudo-todo-app/databases/(default)/documents/';
+
+  static const apiKey = 'AIzaSyAFZ2uyHiPuQQi_4wf8cUQpdMEMqGapq_Q';
+
+  static Uri baseUri = Uri(scheme: scheme, host: host, path: _extension);
+
+  static Uri categories =
+      baseUri.replace(path: '$_extension/categories?key=$apiKey');
+
+  static Uri task = baseUri.replace(path: '$_extension/tasks?key=$apiKey');
+
+  static Uri updateTask(String id) => baseUri.replace(
+    path: '$_extension/tasks/id?key=$apiKey',
+  );
 }
